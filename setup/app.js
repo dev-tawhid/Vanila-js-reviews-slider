55dc6655e96a42af5330230788b91003783c8aef
unknown <towhid2302@gmail.com>
2021-09-27 02:21:58 +0600

    review slider

--- setup/app.js
+++ setup/app.js
@@ -36,4 +36,61 @@ const reviews = [
     text:
       "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
   },
+
 ];
+
+
+//select item
+
+const img = document.getElementById('person-img');
+const  author = document.getElementById('author');
+const job = document.getElementById('job');
+const info = document.getElementById('info');
+
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+
+//set initial item
+
+let currentItem = 0;
+
+//load initial item
+window.addEventListener("DOMContentLoaded",function(){
+  showPerson();
+});
+
+function showPerson(){
+  const item = reviews[currentItem];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+}
+
+//show next button
+nextBtn.addEventListener('click',function(){
+  currentItem++;
+  if(currentItem > reviews.length -1){
+    currentItem = 0;
+  }
+  showPerson(currentItem);
+});
+
+prevBtn.addEventListener('click',function(){
+  currentItem--;
+  if(currentItem < 0){
+    currentItem = reviews.length -1;
+  }
+  showPerson(currentItem);
+});
+
+
+
+
+
+
+
+
+
+
+
